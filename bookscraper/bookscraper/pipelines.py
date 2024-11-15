ac136de108e36ad7d89795ae0044dd885c9459a5
MaleeshaAluwihare <159033744+MaleeshaAluwihare@users.noreply.github.com>
2024-11-15 20:44:30 +0530

    Update pipelines.py

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -10,4 +10,20 @@ from itemadapter import ItemAdapter
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
-        return item
+
+        adapter =  ItemAdapter(item)
+
+        # Strip all the whitespace from strings
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                adapter[field_name] = value.strip()
+
+        # category and product type -- switch to lower case
+        lowercase_keys = ['category', 'product_type']
+        for lowercase_key in lowercase_keys:
+            value = adapter.get(lowercase_key)
+            adapter[lowercase_key] = value.lower()
+            
+        return item, 
